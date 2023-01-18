d3491d59d4865ff94d6beaaa068c8923ee7252d1
Oskar Gierszewski <oskar@moodup.team>
2023-01-18 16:04:36 +0100

    Add navigation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { HomeScreen, ProfileScreen } from './src/screens/HomeScreen';
+import StatusBar from './src/shared/components/StatusBar';
+import { SafeAreaProvider a} from 'react-native-safe-area-context';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <StatusBar />
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false
+          }}
+        >
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Profile" component={ProfileScreen} />
+        </Stack.Navigator>
+        <View style={styles.container}>
+          <Text>Open up App.tsx to start working on your app!</Text>
+          {/* <StatusBar style="auto" /> */}
+        </View>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
@@ -15,6 +34,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
