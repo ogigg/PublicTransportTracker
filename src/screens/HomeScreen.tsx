e54752364406ea3e41fec9717d70687978a5839e
Oskar Gierszewski <oskar@moodup.team>
2023-01-19 11:12:05 +0100

    Add material ui

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,22 @@
-import { Button } from "react-native";
+import { Button, View } from "react-native";
 import {Text, StyleSheet} from 'react-native';
+import { Button as ButtonMD } from 'react-native-paper';
+
 
 export const HomeScreen = ({navigation}) => {
     return (
-      <Button
-        title="Go to Oskar's profile"
-        onPress={() =>
-          navigation.navigate('Profile', {name: 'Jane'})
-        }
-      />
+      <View>
+        <Button
+          title="Go to Oskar's profile"
+          onPress={() =>
+            navigation.navigate('Profile', {name: 'Jane'})
+          }
+        />
+        <ButtonMD icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+          Press me
+        </ButtonMD>
+      </View>
+      
     );
   };
   export const ProfileScreen = ({navigation, route}) => <Text>This is Oskar's profile</Text>
