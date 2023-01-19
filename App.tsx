727d989e08bf02fc955deb3b0234824bcf3248c0
Oskar Gierszewski <oskar@moodup.team>
2023-01-19 11:39:36 +0100

    Add bottom navigation

--- App.tsx
+++ App.tsx
@@ -15,9 +15,7 @@ export default function App() {
       <SafeAreaProvider>
         <NavigationContainer>
           <StatusBar />
-          <TabNavigator />
-          <BottomSafeArea />
-          
+          <TabNavigator />        
         </NavigationContainer>
       </SafeAreaProvider>
     </CustomThemeProvider>
