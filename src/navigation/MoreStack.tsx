b6a75b6f2aaeb99c3c6b502a50741980806f919d
Oskar Gierszewski <oskar@moodup.team>
2023-01-21 20:14:13 +0100

    Add resetting navigation stack on blur

--- src/navigation/MoreStack.tsx
+++ src/navigation/MoreStack.tsx
@@ -1,9 +1,21 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { SettingsScreen } from "../screens/SettingsScreen";
 import { MoreScreen } from "../screens/MoreScreen";
+import { useEffect } from "react";
+import { NavigationProp } from "@react-navigation/native";
+import { CommonActions } from '@react-navigation/native';
 
-export const MoreStack = ({navigation}) => {
+export const MoreStack = ({navigation}: { navigation: NavigationProp<any> }) => {
   const Stack = createNativeStackNavigator();
+
+  useEffect(() => {
+    
+    const unsubscribe = navigation.addListener('blur', () => {
+        navigation.dispatch(CommonActions.reset({ index: 0, routes: [{ name: 'More' }]}))
+    });
+
+    return unsubscribe;
+  }, [navigation]);
 
     return (
         <Stack.Navigator initialRouteName="More">
