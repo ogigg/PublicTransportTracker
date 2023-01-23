6af9b26d9c0cdb420faf3474a7e8da37d2b5fe8e
Oskar Gierszewski <oskar@moodup.team>
2023-01-23 07:54:50 +0100

    Add setting theme via redux

--- src/screens/SettingsScreen/components/ThemePicker.tsx
+++ src/screens/SettingsScreen/components/ThemePicker.tsx
@@ -1,21 +1,31 @@
 import { View } from "react-native";
 import { useTranslation } from "react-i18next";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Menu, Divider, Button, Dialog, Portal, Text, RadioButton } from "react-native-paper";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { selectTheme, setTheme } from "../../../store/settings/settingsSlice";
+import { ThemeOption } from "../../../models/settings";
 
 export const ThemePicker = () => {
   const { t } = useTranslation();
   const [visible, setVisible] = useState(false);
-  const [selectedValue, setSelectedValue] = useState('light');
+  const [selectedValue, setSelectedValue] = useState<ThemeOption>('light');
+  const theme = useSelector(selectTheme);
   const showDialog = () => setVisible(true);
   const hideDialog = () => setVisible(false);
   const dispatch = useDispatch();
-  const onThemePicked =(themeName) => {
-    console.log(themeName);
+
+  useEffect(() => {
+    setSelectedValue(theme);
+  }, [theme])
+  
+  const onThemePicked =(themeName: ThemeOption) => {
     setSelectedValue(themeName);
+    dispatch(setTheme(themeName))
     hideDialog();
   }
+
+
     return (
         <View>
         <Menu.Item leadingIcon='brightness-6' onPress={showDialog} title={t('settingsScreen.theme.label')} style={{ maxWidth: '100%' }}></Menu.Item>
@@ -23,7 +33,7 @@ export const ThemePicker = () => {
           <Dialog visible={visible} onDismiss={hideDialog}>
             <Dialog.Title>{t('settingsScreen.theme.title')}</Dialog.Title>
             <Dialog.Content>
-                <RadioButton.Group onValueChange={value => onThemePicked(value)} value={selectedValue}>
+                <RadioButton.Group onValueChange={(value: ThemeOption) => onThemePicked(value)} value={selectedValue}>
                     <RadioButton.Item label={t('settingsScreen.theme.light')} value="light" />
                     <RadioButton.Item label={t('settingsScreen.theme.dark')} value="dark" />
                 </RadioButton.Group>
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
+import settingsSlice from './settings/settingsSlice'
 import stopSlice from './stop/stopSlice'
 
 export const store = configureStore({
-  reducer: {stop: stopSlice},
+  reducer: {stop: stopSlice, settings: settingsSlice},
 })
 
 export type RootState = ReturnType<typeof store.getState>
