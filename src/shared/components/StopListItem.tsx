2f911fbb348e82551c972a8e30a6a7a1d035561a
Oskar Gierszewski <oskar@moodup.team>
2023-01-27 16:02:31 +0100

    Add expandable list item

--- src/shared/components/StopListItem.tsx
+++ src/shared/components/StopListItem.tsx
@@ -1,17 +1,32 @@
-import React from 'react';
-import { View, StatusBar, Platform } from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import { List, MD3Colors, Text, TouchableRipple } from 'react-native-paper';
+
 import { Stop } from '../../models/stop';
-import { Text } from "react-native-paper";
 
 interface StopListItemProps {
     stop: Stop;
 }
 export const StopListItem = ({ stop }: StopListItemProps) => {
+  const [expanded, setIsExpanded] = useState<boolean>(false);
 
-
-  return (<View>
-    <Text>{stop.name}</Text>
-    <Text>{stop.code}</Text>
-    </View>);
+  const expand = () => {
+    setIsExpanded(!expanded);
+  }
+  return (
+    <TouchableRipple onPress={expand}>
+      <View className="p-3">
+        <View className="flex-row">
+          <View className="flex-row flex-1 justify-between">
+            <Text>{stop.name}</Text>
+            <Text>{stop.code}</Text>
+          </View>
+          <View className="pl-3">
+            <List.Icon color={MD3Colors.tertiary70} icon={expanded ? "chevron-up" : "chevron-down"} />
+          </View>
+        </View>
+        {expanded && <View><Text>More Info TODO</Text></View>}
+      </View>
+    </TouchableRipple>
+  );
 }
